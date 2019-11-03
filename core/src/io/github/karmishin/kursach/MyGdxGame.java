b97be86e85bd4b1c28a123256fb24af4caf14129
Mikhail Karmishin <karmishinru@gmail.com>
2019-11-03 21:44:21 +0300

    create player class

--- core/src/io/github/karmishin/kursach/MyGdxGame.java
+++ core/src/io/github/karmishin/kursach/MyGdxGame.java
@@ -8,27 +8,21 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Rectangle;
-
-import java.awt.*;
 
 public class MyGdxGame extends ApplicationAdapter {
+	Player player;
 	SpriteBatch batch;
-	Texture img;
-	Sprite sprite;
-	Rectangle rectangle;
 	Music music;
+
+	public MyGdxGame() {
+		player = new Player();
+	}
 	
 	@Override
 	public void create () {
+		player.texture = new Texture(Gdx.files.internal("character/sprites/idle outline.gif"));
+		player.sprite = new Sprite(player.texture);
 		batch = new SpriteBatch();
-		img = new Texture("character/sprites/idle outline.gif");
-		sprite = new Sprite(img);
-		rectangle = new Rectangle();
-		rectangle.x = 400;
-		rectangle.y = 20;
-		rectangle.width = 21;
-		rectangle.height = 35;
 
 		music = Gdx.audio.newMusic(Gdx.files.internal("music/golosovanie.mp3"));
 		music.setVolume(0);
@@ -40,21 +34,21 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(sprite, rectangle.x, rectangle.y);
+		batch.draw(player.sprite, player.rectangle.x, player.rectangle.y);
 		batch.end();
 
 		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-			rectangle.x -= 200 * Gdx.graphics.getDeltaTime();
+			player.rectangle.x -= 100 * Gdx.graphics.getDeltaTime();
 		}
 
 		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-			rectangle.x += 200 * Gdx.graphics.getDeltaTime();
+			player.rectangle.x += 100 * Gdx.graphics.getDeltaTime();
 		}
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		player.texture.dispose();
 	}
 }
---
+++ core/src/io/github/karmishin/kursach/Player.java
@@ -0,0 +1,21 @@
+package io.github.karmishin.kursach;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Rectangle;
+
+public class Player {
+    Texture texture;
+    Sprite sprite;
+    Rectangle rectangle;
+
+    Player() {
+        rectangle = new Rectangle();
+
+        rectangle.x = 400;
+        rectangle.y = 20;
+        rectangle.width = 21;
+        rectangle.height = 35;
+    }
+}
