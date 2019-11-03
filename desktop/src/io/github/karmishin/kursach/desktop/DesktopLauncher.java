696e9341b74828c1acdcaa8266ab178f274fb608
Mikhail Karmishin <karmishinru@gmail.com>
2019-11-03 22:18:28 +0300

    set background size, add camera

--- desktop/src/io/github/karmishin/kursach/desktop/DesktopLauncher.java
+++ desktop/src/io/github/karmishin/kursach/desktop/DesktopLauncher.java
@@ -9,7 +9,7 @@ public class DesktopLauncher {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Kursach: The Game";
 		config.width = 800;
-		config.height = 600;
+		config.height = 480;
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
