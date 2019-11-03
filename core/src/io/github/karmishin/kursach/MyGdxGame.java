27f74e24aefeb18caf40fd89c2c563015ce24b87
Mikhail Karmishin <karmishinru@gmail.com>
2019-11-03 22:24:41 +0300

    add remaining background sprites

--- core/src/io/github/karmishin/kursach/MyGdxGame.java
+++ core/src/io/github/karmishin/kursach/MyGdxGame.java
@@ -18,7 +18,6 @@ public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Music music;
 
-
 	public MyGdxGame() {
 		player = new Player();
 		background = new Background();
@@ -27,7 +26,16 @@ public class MyGdxGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		background.texture1 = new Texture(Gdx.files.internal("background/plx-1.png"));
+		background.texture2 = new Texture(Gdx.files.internal("background/plx-2.png"));
+		background.texture3 = new Texture(Gdx.files.internal("background/plx-3.png"));
+		background.texture4 = new Texture(Gdx.files.internal("background/plx-4.png"));
+		background.texture5 = new Texture(Gdx.files.internal("background/plx-5.png"));
+
 		background.sprite1 = new Sprite(background.texture1);
+		background.sprite2 = new Sprite(background.texture2);
+		background.sprite3 = new Sprite(background.texture3);
+		background.sprite4 = new Sprite(background.texture4);
+		background.sprite5 = new Sprite(background.texture5);
 
 		player.texture = new Texture(Gdx.files.internal("character/sprites/idle outline.gif"));
 		player.sprite = new Sprite(player.texture);
@@ -47,6 +55,10 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(background.sprite1, 0, 0, 800, 480);
+		batch.draw(background.sprite2, 0, 0, 800, 480);
+		batch.draw(background.sprite3, 0, 0, 800, 480);
+		batch.draw(background.sprite4, 0, 0, 800, 480);
+		batch.draw(background.sprite5, 0, 0, 800, 480);
 		batch.draw(player.sprite, player.rectangle.x, player.rectangle.y);
 		batch.end();
 
