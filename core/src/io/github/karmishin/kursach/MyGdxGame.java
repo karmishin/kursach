b4511d70f26afdf34f089b041f2e29e51c6bf859
Mikhail Karmishin <karmishinru@gmail.com>
2019-11-03 21:13:31 +0300

    move character, set music volume to 0

--- core/src/io/github/karmishin/kursach/MyGdxGame.java
+++ core/src/io/github/karmishin/kursach/MyGdxGame.java
@@ -8,11 +8,15 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+
+import java.awt.*;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	Sprite sprite;
+	Rectangle rectangle;
 	Music music;
 	
 	@Override
@@ -20,8 +24,14 @@ public class MyGdxGame extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		img = new Texture("character/sprites/idle outline.gif");
 		sprite = new Sprite(img);
+		rectangle = new Rectangle();
+		rectangle.x = 400;
+		rectangle.y = 20;
+		rectangle.width = 21;
+		rectangle.height = 35;
 
 		music = Gdx.audio.newMusic(Gdx.files.internal("music/golosovanie.mp3"));
+		music.setVolume(0);
 		music.play();
 	}
 
@@ -30,11 +40,15 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(sprite, 0, 0);
+		batch.draw(sprite, rectangle.x, rectangle.y);
 		batch.end();
 
 		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			rectangle.x -= 200 * Gdx.graphics.getDeltaTime();
+		}
 
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			rectangle.x += 200 * Gdx.graphics.getDeltaTime();
 		}
 	}
 	
