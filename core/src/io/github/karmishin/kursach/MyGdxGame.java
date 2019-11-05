2660b9fea77a8bbc0078e7f50873ec65b7d98384
Mikhail Karmishin <karmishinru@gmail.com>
2019-11-05 11:34:20 +0300

    background, trying to make animation work

--- core/src/io/github/karmishin/kursach/MyGdxGame.java
+++ core/src/io/github/karmishin/kursach/MyGdxGame.java
@@ -7,13 +7,13 @@ import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.*;
 
 public class MyGdxGame extends ApplicationAdapter {
 	Background background;
 	Player player;
-
+	Animation<TextureRegion> runningAnimation;
+	TextureAtlas atlas;
 	OrthographicCamera camera;
 	SpriteBatch batch;
 	Music music;
@@ -22,15 +22,28 @@ public class MyGdxGame extends ApplicationAdapter {
 	public MyGdxGame() {
 		player = new Player();
 		background = new Background();
+
 	}
 	
 	@Override
 	public void create () {
-			background.texture1 = new Texture(Gdx.files.internal("background/plx-1.png"));
-			background.sprite1 = new Sprite(background.texture1);
+		atlas = new TextureAtlas(Gdx.files.internal("character/sprites/run.atlas"));
+		runningAnimation = new Animation<TextureRegion>(0.033f, atlas.getRegions());
 
-			player.texture = new Texture(Gdx.files.internal("character/sprites/idle outline.gif"));
-			player.sprite = new Sprite(player.texture);
+		background.texture1 = new Texture(Gdx.files.internal("background/plx-1.png"));
+		background.texture2 = new Texture(Gdx.files.internal("background/plx-2.png"));
+		background.texture3 = new Texture(Gdx.files.internal("background/plx-3.png"));
+		background.texture4 = new Texture(Gdx.files.internal("background/plx-4.png"));
+		background.texture5 = new Texture(Gdx.files.internal("background/plx-5.png"));
+
+		background.sprite1 = new Sprite(background.texture1);
+		background.sprite2 = new Sprite(background.texture2);
+		background.sprite3 = new Sprite(background.texture3);
+		background.sprite4 = new Sprite(background.texture4);
+		background.sprite5 = new Sprite(background.texture5);
+
+		player.texture = new Texture(Gdx.files.internal("character/sprites/stay/frame-01.png"));
+		player.sprite = new Sprite(player.texture);
 
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 800, 480);
@@ -47,6 +60,10 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(background.sprite1, 0, 0, 800, 480);
+		batch.draw(background.sprite2, 0, 0, 800, 480);
+		batch.draw(background.sprite3, 0, 0, 800, 480);
+		batch.draw(background.sprite4, 0, 0, 800, 480);
+		batch.draw(background.sprite5, 0, 0, 800, 480);
 		batch.draw(player.sprite, player.rectangle.x, player.rectangle.y);
 		batch.end();
 
