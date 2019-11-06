612bd2b9af94f3d69944c164410377ad1298f641
Mikhail Karmishin <karmishinru@gmail.com>
2019-11-06 11:18:24 +0300

    LWJGL 3 support

--- desktop/src/io/github/karmishin/kursach/desktop/DesktopLauncher.java
+++ desktop/src/io/github/karmishin/kursach/desktop/DesktopLauncher.java
@@ -1,15 +1,14 @@
 package io.github.karmishin.kursach.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import io.github.karmishin.kursach.MyGdxGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Kursach: The Game";
-		config.width = 800;
-		config.height = 480;
-		new LwjglApplication(new MyGdxGame(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("Kursach: The Game");
+		config.setWindowedMode(800, 480);
+		new Lwjgl3Application(new MyGdxGame(), config);
 	}
 }
