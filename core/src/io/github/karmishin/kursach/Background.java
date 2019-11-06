b9c28ab4c56ee23554ba4d67dccb8d3fa226e696
Mikhail Karmishin <karmishinru@gmail.com>
2019-11-06 11:54:56 +0300

    move background to a separate class

--- core/src/io/github/karmishin/kursach/Background.java
+++ core/src/io/github/karmishin/kursach/Background.java
@@ -1,9 +1,24 @@
 package io.github.karmishin.kursach;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
 class Background {
-     Texture texture1, texture2, texture3, texture4, texture5;
+     private Texture texture1, texture2, texture3, texture4, texture5;
      Sprite sprite1, sprite2, sprite3, sprite4, sprite5;
+
+     Background() {
+          texture1 = new Texture(Gdx.files.internal("background/plx-1.png"));
+          texture2 = new Texture(Gdx.files.internal("background/plx-2.png"));
+          texture3 = new Texture(Gdx.files.internal("background/plx-3.png"));
+          texture4 = new Texture(Gdx.files.internal("background/plx-4.png"));
+          texture5 = new Texture(Gdx.files.internal("background/plx-5.png"));
+
+          sprite1 = new Sprite(texture1);
+          sprite2 = new Sprite(texture2);
+          sprite3 = new Sprite(texture3);
+          sprite4 = new Sprite(texture4);
+          sprite5 = new Sprite(texture5);
+     }
 }
--- core/src/io/github/karmishin/kursach/MyGdxGame.java
+++ core/src/io/github/karmishin/kursach/MyGdxGame.java
@@ -10,21 +10,15 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.*;
 
 public class MyGdxGame extends ApplicationAdapter {
-	Background background;
-	Player player;
-	Animation<TextureRegion> runningAnimation, idleAnimation;
-	TextureAtlas runningAtlas, idleAtlas;
-	OrthographicCamera camera;
-	SpriteBatch batch;
-	Music music;
+    private Background background;
+	private Player player = new Player();
+	private Animation<TextureRegion> runningAnimation, idleAnimation;
+	private TextureAtlas runningAtlas, idleAtlas;
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+	private Music music;
 	private float elapsedTime = 0;
 
-
-	public MyGdxGame() {
-		player = new Player();
-		background = new Background();
-
-	}
 	
 	@Override
 	public void create () {
@@ -33,19 +27,9 @@ public class MyGdxGame extends ApplicationAdapter {
 		idleAtlas = new TextureAtlas(Gdx.files.internal("character/sprites/idle.atlas"));
 		idleAnimation = new Animation<TextureRegion>(0.1f, idleAtlas.getRegions());
 
-		background.texture1 = new Texture(Gdx.files.internal("background/plx-1.png"));
-		background.texture2 = new Texture(Gdx.files.internal("background/plx-2.png"));
-		background.texture3 = new Texture(Gdx.files.internal("background/plx-3.png"));
-		background.texture4 = new Texture(Gdx.files.internal("background/plx-4.png"));
-		background.texture5 = new Texture(Gdx.files.internal("background/plx-5.png"));
+        background = new Background();
 
-		background.sprite1 = new Sprite(background.texture1);
-		background.sprite2 = new Sprite(background.texture2);
-		background.sprite3 = new Sprite(background.texture3);
-		background.sprite4 = new Sprite(background.texture4);
-		background.sprite5 = new Sprite(background.texture5);
-
-		player.texture = new Texture(Gdx.files.internal("character/sprites/stay/frame-01.png"));
+        player.texture = new Texture(Gdx.files.internal("character/sprites/stay/frame-01.png"));
 		player.sprite = new Sprite();
 
 		camera = new OrthographicCamera();
@@ -63,15 +47,13 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		elapsedTime += Gdx.graphics.getDeltaTime();
 		TextureRegion currentFrame = idleAnimation.getKeyFrame(elapsedTime, true);
-
+        new Background();
 		batch.begin();
 		batch.draw(background.sprite1, 0, 0, 800, 480);
 		batch.draw(background.sprite2, 0, 0, 800, 480);
 		batch.draw(background.sprite3, 0, 0, 800, 480);
 		batch.draw(background.sprite4, 0, 0, 800, 480);
 		batch.draw(background.sprite5, 0, 0, 800, 480);
-		//batch.draw(player.sprite, player.rectangle.x, player.rectangle.y);
-
 		batch.draw(currentFrame, player.rectangle.x, player.rectangle.y);
 		batch.end();
 
